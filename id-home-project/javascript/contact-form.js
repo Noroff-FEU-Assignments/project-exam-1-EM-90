67bc5078b0bdda1f0b06983075579ce03c472518
Eirik Michielsen <eirik.michielsen@gmail.com>
2023-02-01 11:55:53 +0100

    validation function
    validateContactForm works as it should

--- id-home-project/javascript/contact-form.js
+++ id-home-project/javascript/contact-form.js
@@ -3,16 +3,25 @@ const fullName = document.querySelector("#name");
 const email = document.querySelector("#e-mail");
 const subject = document.querySelector("#subject");
 const fullNameError = document.querySelector("#fullNameError");
+const emailError = document.querySelector("#emailError");
+const subjectError = document.querySelector("#subjectError");
 
 function validateContactForm() {
   event.preventDefault();
 
-  if (fullName.value.lenght > 0) {
+  if (fullName.value.trim().length > 0) {
     fullNameError.style.display = "none";
-  } else {
-    fullNameError.style.display = "block";
-  }
-  console.log("Hello");
+  } else fullNameError.style.display = "block";
+
+  if (email.value.trim().length > 0) {
+    emailError.style.display = "none";
+  } else emailError.style.display = "block";
+
+  if (subject.value.length > 5) {
+    subjectError.style.display = "none";
+  } else subjectError.style.display = "block";
+
+  console.log(fullName.value);
 }
 
 form.addEventListener("submit", validateContactForm);
