eabdbe990d249e018e55cee87d954432b47faf62
Eirik Michielsen <eirik.michielsen@gmail.com>
2023-02-02 00:41:42 +0100

    making button function

--- id-home-project/javascript/contact-form.js
+++ id-home-project/javascript/contact-form.js
@@ -5,19 +5,20 @@ const subject = document.querySelector("#subject");
 const fullNameError = document.querySelector("#fullNameError");
 const emailError = document.querySelector("#emailError");
 const subjectError = document.querySelector("#subjectError");
+const 
 
 function validateContactForm() {
   event.preventDefault();
 
-  if (fullName.value.trim().length > 0) {
+  if (checkLength(fullName.value, 5) === true) {
     fullNameError.style.display = "none";
   } else fullNameError.style.display = "block";
 
-  if (email.value.trim().length > 0) {
+  if (validateEmail(email.value) === true) {
     emailError.style.display = "none";
   } else emailError.style.display = "block";
 
-  if (subject.value.length > 5) {
+  if (checkLength(subject.value, 15) === true) {
     subjectError.style.display = "none";
   } else subjectError.style.display = "block";
 
@@ -25,3 +26,23 @@ function validateContactForm() {
 }
 
 form.addEventListener("submit", validateContactForm);
+
+function validateEmail(email) {
+  const regEx = /\S+@\S+\.\S+/;
+  const patternMatches = regEx.test(email);
+  return patternMatches;
+}
+
+function checkLength(value, len) {
+  if (value.trim().length > len) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function enableButton(){
+  if (checkLength(fullName.value, 5) === true  && validateEmail(email.value) === true && checkLength(subject.value, 15)){
+    button.disabled = false;
+  } 
+}
