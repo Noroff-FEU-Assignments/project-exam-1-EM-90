99f9c80df1616160b6cb06d5e44ad7f020543294
Eirik Michielsen <eirik.michielsen@gmail.com>
2023-02-07 18:52:19 +0100

    fetched posts

---
+++ id-home-project/javascript/api.js
@@ -0,0 +1,26 @@
+const apiUrl = "https://emdevelopment.no/Project-exam-1/wp-json/wp/v2/posts";
+const container = document.querySelector(".blog-post-container");
+let blogPostList = "";
+
+async function fetchApi() {
+  const response = await fetch(apiUrl);
+  const result = response.json();
+  const data = await result;
+
+  console.log(data);
+
+  data.forEach((post) => {
+    console.log(post);
+
+    blogPostList += `
+                    <div class="post-block">
+                     <h2 class="post-title">${post.title.rendered}</h2>
+                     <p class="post-text">${post.excerpt.rendered}</p>
+                    </div>
+                   `;
+
+    container.innerHTML = blogPostList;
+  });
+}
+
+fetchApi();
