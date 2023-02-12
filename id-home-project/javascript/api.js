8ef02f87a6adbe03f60c4a10d1c858ef110afd9b
Eirik Michielsen <eirik.michielsen@gmail.com>
2023-02-12 22:30:39 +0100

    GetMoreBlogPosts

--- id-home-project/javascript/api.js
+++ id-home-project/javascript/api.js
@@ -1,29 +1,41 @@
-const apiUrl = "https://emdevelopment.no/Project-exam-1/wp-json/wp/v2/posts";
+const apiUrl = "https://emdevelopment.no/Project-exam-1/wp-json/wp/v2/posts?";
 const container = document.querySelector(".blog-post-container");
-const buttonLink =
-  "https://emdevelopment.no/Project-exam-1/wp-json/wp/v2/posts?per_page=20";
-const postBtn = document.querySelector(".post-btn");
+const postButton = document.querySelector(".post-btn");
+let amountOfPages = 10;
 let blogPostList = "";
 
-async function fetchApi() {
-  const response = await fetch(apiUrl);
-  const result = response.json();
+async function GetBlogPosts(amountOfPages) {
+  const response = await fetch(`${apiUrl}&per_page=${amountOfPages}`);
+  const result = await response.json();
   const data = await result;
+  return data;
+}
 
-  console.log(data);
-
+const AddBlogPostToHtml = (data) => {
   data.forEach((post) => {
-    console.log(post);
-
     blogPostList += `
                     <div class="post-block">
                      <h2 class="post-title">${post.title.rendered}</h2>
                      <p class="post-text">${post.excerpt.rendered}</p>
                     </div>
                    `;
-
     container.innerHTML = blogPostList;
   });
-}
+};
 
-fetchApi();
+const OnMounted = async () => {
+  const data = await GetBlogPosts(amountOfPages);
+  AddBlogPostToHtml(data);
+};
+
+// initialise
+OnMounted();
+
+postButton.addEventListener("click", async () => {
+  // return if amountOfPages of 20 allready has been fetched.
+  if (amountOfPages >= 20) return;
+
+  amountOfPages = 20;
+  const data = await GetBlogPosts(amountOfPages);
+  AddBlogPostToHtml(data);
+});
