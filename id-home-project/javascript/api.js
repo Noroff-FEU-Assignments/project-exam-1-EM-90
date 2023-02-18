ec2c0a6fd1051cd6b8130712935db50ce2c8d83b
Eirik Michielsen <eirik.michielsen@gmail.com>
2023-02-18 19:04:36 +0100

    carousel

--- id-home-project/javascript/api.js
+++ id-home-project/javascript/api.js
@@ -48,3 +48,40 @@ postButton.addEventListener("click", async () => {
   const data = await GetBlogPosts(amountOfPosts);
   AddBlogPostToHtml(data);
 });
+
+const carousel = document.querySelector(".carousel-container");
+const nextButton = document.querySelector(".right-handel");
+const previousButton = document.querySelector(".left-handel");
+const slider = document.querySelector(".slider");
+const cards = document.querySelectorAll(".featured-block");
+console.log(cards);
+
+let slideIndex = 0;
+
+nextButton.addEventListener("click", () => {
+  console.log("next button clicked");
+  slideIndex += 4;
+  displayCards();
+});
+
+previousButton.addEventListener("click", () => {
+  console.log("previous button clicked");
+  slideIndex -= 4;
+  displayCards();
+});
+
+function displayCards() {
+  if (slideIndex >= cards.length) {
+    slideIndex = 0;
+  } else if (slideIndex < 0) {
+    slideIndex = Math.max(0, cards.length - 4);
+  }
+
+  cards.forEach((card) => {
+    card.style.display = "none";
+  });
+
+  for (let i = slideIndex; i < slideIndex + 4 && i < cards.length; i++) {
+    cards[i].style.display = "block";
+  }
+}
--- id-home-project/javascript/carousel.js
+++ id-home-project/javascript/carousel.js
@@ -1,1 +1,37 @@
+const carousel = document.querySelector(".carousel-container");
+const nextButton = document.querySelector(".right-handle");
+const previousButton = document.querySelector(".left-handle");
+const slider = document.querySelector(".slider");
+const cards = document.querySelectorAll(".featured-block");
 
+let slideIndex = 0;
+
+function displayCards() {
+  if (slideIndex >= cards.length) {
+    slideIndex = 0;
+  } else if (slideIndex < 0) {
+    slideIndex = Math.max(0, cards.length - 4);
+  }
+
+  cards.forEach((card) => {
+    card.style.display = "none";
+  });
+
+  for (let i = slideIndex; i < slideIndex + 4 && i < cards.length; i++) {
+    cards[i].style.display = "flex";
+  }
+}
+
+nextButton.addEventListener("click", () => {
+  console.log("next button clicked");
+  slideIndex += 4;
+  displayCards();
+});
+
+previousButton.addEventListener("click", () => {
+  console.log("previous button clicked");
+  slideIndex -= 4;
+  displayCards();
+});
+
+displayCards();
