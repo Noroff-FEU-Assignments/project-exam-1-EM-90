ba78122f116c8cb02aaaf590ab67227e886e02f7
Eirik Michielsen <eirik.michielsen@gmail.com>
2023-02-19 18:21:20 +0100

    Carousel
    -function works, but overflow is an issue

--- id-home-project/javascript/featured-posts.js
+++ id-home-project/javascript/featured-posts.js
@@ -1,5 +1,9 @@
 let featuredData = "";
 let featuredContainer = document.querySelector(".slider");
+const carousel = document.querySelector(".carousel-container");
+const nextButton = document.querySelector(".right-handle");
+const previousButton = document.querySelector(".left-handle");
+let cards = document.querySelectorAll(".featured-block");
 
 async function GetFeaturedPosts() {
   const response = await fetch(
@@ -22,12 +26,70 @@ const AddFeaturedPosts = (data) => {
                               </div>
                             </a>
                              `;
-    featuredContainer.innerHTML = featuredData;
   }
+  featuredContainer.innerHTML = featuredData;
+  cards = document.querySelectorAll(".featured-block");
 };
+
+function Carousel() {
+  const cardWidth = cards[0].offsetWidth;
+  let slideIndex = 0;
+
+  function displayCards() {
+    featuredContainer.style.transform = `translateX(-${
+      slideIndex * cardWidth
+    }px)`;
+  }
+
+  nextButton.addEventListener("click", () => {
+    slideIndex = (slideIndex + 4) % cards.length;
+    displayCards();
+  });
+
+  previousButton.addEventListener("click", () => {
+    slideIndex = (slideIndex - 4 + cards.length) % cards.length;
+    displayCards();
+  });
+
+  window.addEventListener("resize", displayCards);
+}
 
 const OnMounted = async () => {
   const data = await GetFeaturedPosts();
   AddFeaturedPosts(data);
+  Carousel();
 };
+
 OnMounted();
+
+/*function Carousel() {
+  const cardWidth = cards[0].offsetWidth;
+  let slideIndex = 0;
+
+  function displayCards() {
+    featuredContainer.style.transform = `translateX(-${
+      slideIndex * cardWidth
+    }px)`;
+  }
+
+  nextButton.addEventListener("click", () => {
+    slideIndex += 4;
+    if (slideIndex > cards.length - 4) {
+      slideIndex = 0;
+      console.log(slideIndex);
+    }
+    displayCards();
+  });
+
+  previousButton.addEventListener("click", () => {
+    slideIndex -= 4;
+    if (slideIndex < 0) {
+      slideIndex = cards.length - 4;
+    }
+    displayCards();
+  });
+
+  window.addEventListener("resize", (cardWidth) => {
+    displayCards();
+  });
+}*/
